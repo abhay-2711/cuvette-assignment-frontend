7b9223db9937881946325cefd809c743f57e8a06
abhay-zel <abhay.pratap@zel.finance>
2024-02-09 19:14:58 +0530

    protected route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 import Header from './components/Header/Header';
 import Column from './components/Column/Column';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router';
 
 function App() {
+  const { currentUser } = useSelector((state) => state.user);
+
+  const isAuthenticated = () => {
+    if(currentUser){
+      return true;
+    }
+    return false;
+  };
+  
+  const ProtectedRoute = ({ element, ...rest }) => {
+    if (!isAuthenticated()) {
+      return <Navigate to="/" replace />;
+    }
+  
+    return React.cloneElement(element, rest);
+  };
+
   return (
     <div>
       <BrowserRouter>
@@ -13,7 +33,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SignIn/>} />
           <Route path="/signup" element={<SignUp/>} />
-          <Route path="/todo" element={<Column/>} />
+          <Route path="/todo" element={<ProtectedRoute element={<Column />} />} />
         </Routes>
       </BrowserRouter>
     </div>
